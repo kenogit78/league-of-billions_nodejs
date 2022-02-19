f8aec2a375ab5d5c9cd5a2b73ab3af8f1a3c94d6
Paul Keno <adarojepaulo@gmail.com>
2022-02-19 12:30:17 +0100

    procfile

---
+++ email/sendgrid/index.js
@@ -0,0 +1,25 @@
+const sendgrid = require('@sendgrid/mail')
+const config = require('../config')
+
+class Sendgrid {
+    constructor() {
+        sendgrid.setApiKey(config.sendgrid)
+    }
+
+    send(data) {
+        const { subject, to, payload } = data
+        const msg = {
+            to, 
+            from: config.sendgridEmail,
+            subject,
+            templateId: config.dynamicTemplateId, 
+            dynamic_template_data: {
+                subject,
+                payload: payload.otp
+            }      
+        }
+        return sendgrid.send(msg)
+    }
+}
+
+module.exports = new Sendgrid()
